dab9aaf6fb26b069583a34ae56023750bbab5050
williamprieto <willopv@gmail.com>
2016-08-09 20:53:09 -0500

    ciudades y departamentos

--- app/assets/javascripts/ciudades.js
+++ app/assets/javascripts/ciudades.js
@@ -5,7 +5,7 @@ function loadJSON(callback) {
 
     var xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'colombia-json.json', true); // Reemplaza colombia-json.json con el nombre que le hayas puesto
+    xobj.open('GET', "http://"+window.location.host +'/colombia.json', true); // Reemplaza colombia-json.json con el nombre que le hayas puesto
     xobj.onreadystatechange = function () {
         if (xobj.readyState == 4 && xobj.status == "200") {
             callback(xobj.responseText);
@@ -14,10 +14,41 @@ function loadJSON(callback) {
     xobj.send(null);
 }
 
-function init() {
+function init(obj_select) {
     loadJSON(function(response) {
         // Parse JSON string into object
         var JSONFinal = JSON.parse(response);
-        
+        $.each(JSONFinal,function(index, value){
+            $(obj_select).append('<option>'+value.departamento+'</option>');
+        });
     });
 }
+
+function updateCities(){
+    var departamento = $("#event_departamento option:selected").text();
+    loadJSON(function(response) {
+        // Parse JSON string into object
+        var ciudades = [];
+        var JSONFinal = JSON.parse(response);
+        $.each(JSONFinal,function(index, value){
+            if(value.departamento == departamento){
+                ciudades = value.ciudades;
+                return;
+            }
+        });
+        console.log(ciudades);
+        $("#event_lugar").html('');
+        $.each(ciudades,function(index, value){
+            $("#event_lugar").append('<option>'+value+'</option>')
+        });
+    });
+}
+
+$(document).ready(function () {
+    $("#event_departamento").one('mousedown',function () {
+        init(this);
+    });
+    $("#event_departamento").change(function () {
+        updateCities();
+    });
+});
